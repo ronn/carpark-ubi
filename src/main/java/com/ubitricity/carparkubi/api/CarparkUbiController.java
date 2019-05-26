3a6088cf95caee9a51523dfa8550e67b4f1ec976
ronn <ronnie-filth@hotmail.com>
2019-05-26 02:59:06 +0200

    Return Response entities in Controller

--- src/main/java/com/ubitricity/carparkubi/api/CarparkUbiController.java
+++ src/main/java/com/ubitricity/carparkubi/api/CarparkUbiController.java
@@ -1,9 +1,11 @@
 package com.ubitricity.carparkubi.api;
 
-import com.ubitricity.carparkubi.api.model.ChargingPointRecord;
 import com.ubitricity.carparkubi.api.model.Report;
 import com.ubitricity.carparkubi.service.ChargingPointService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class CarparkUbiController {
 
-    private final ChargingPointService cpService;
+    private final ChargingPointService service;
 
     @Autowired
-    public CarparkUbiController(ChargingPointService cpService) {
-        this.cpService = cpService;
+    public CarparkUbiController(ChargingPointService service) {
+        this.service = service;
     }
 
     @RequestMapping("/report")
-    public Report getReport(){
-        return new Report(cpService.getAll());
+    public ResponseEntity<Report> getReport(){
+        return ResponseEntity.ok(new Report(service.getAll()));
     }
 
-    @RequestMapping("/plug")
-    public Report plug(ChargingPointRecord record){
-        return new Report(cpService.getAll());
+    @PutMapping("/plug/{cpId}")
+    public ResponseEntity<Report> plug(@PathVariable String cpId){
+        return service.getChargingPointStream()
+                .filter(chargingPoint -> cpId.equals(chargingPoint.getId().name()))
+                .findFirst()
+                .map(chargingPoint -> {
+                    service.plug(cpId);
+                    return ResponseEntity.ok(new Report(service.getAll()));
+                }).orElse(ResponseEntity.badRequest().build());
     }
 }
