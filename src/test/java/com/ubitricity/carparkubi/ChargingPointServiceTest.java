49001a2cafa6e8622ee606cfb5f1609e8b42a5c0
ronn <ronnie-filth@hotmail.com>
2019-05-26 02:54:04 +0200

    Test total amperes

--- src/test/java/com/ubitricity/carparkubi/ChargingPointServiceTest.java
+++ src/test/java/com/ubitricity/carparkubi/ChargingPointServiceTest.java
@@ -1,4 +1,42 @@
 package com.ubitricity.carparkubi;
 
+import com.ubitricity.carparkubi.domain.ChargingPoint;
+import com.ubitricity.carparkubi.domain.ChargingPointId;
+import com.ubitricity.carparkubi.service.ChargingPointService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
 public class ChargingPointServiceTest {
+
+    @Mock
+    private ChargingPointService service;
+
+    @Before
+    public void init(){
+        service = Mockito.mock(ChargingPointService.class, Mockito.RETURNS_MOCKS);
+        Mockito.when(service.getChargingPointStream())
+                .thenReturn(Stream.of(
+                        new ChargingPoint(ChargingPointId.CP1),
+                        new ChargingPoint(ChargingPointId.CP2),
+                        new ChargingPoint(ChargingPointId.CP3),
+                        new ChargingPoint(ChargingPointId.CP4),
+                        new ChargingPoint(ChargingPointId.CP5),
+                        new ChargingPoint(ChargingPointId.CP6),
+                        new ChargingPoint(ChargingPointId.CP7),
+                        new ChargingPoint(ChargingPointId.CP8),
+                        new ChargingPoint(ChargingPointId.CP9),
+                        new ChargingPoint(ChargingPointId.CP10)
+                ));
+    }
+
+    @Test
+    public void getTotalUsedAmperes(){
+        assertEquals(0, service.getTotalUsedAmperes().intValue());
+    }
 }
