5b6cf6c7d4ccfc81bb3e2a59c804beded175a9e5
ronn <ronnie-filth@hotmail.com>
2019-05-27 21:14:28 +0200

    Change the mocked object to make the server perform its real behavior

--- src/test/java/com/ubitricity/carparkubi/ChargingPointServiceTest.java
+++ src/test/java/com/ubitricity/carparkubi/ChargingPointServiceTest.java
@@ -2,41 +2,49 @@ package com.ubitricity.carparkubi;
 
 import com.ubitricity.carparkubi.domain.ChargingPoint;
 import com.ubitricity.carparkubi.domain.ChargingPointId;
+import com.ubitricity.carparkubi.persistence.ChargingPointRepository;
+import com.ubitricity.carparkubi.persistence.model.ChargingPointDTO;
 import com.ubitricity.carparkubi.service.ChargingPointService;
+import com.ubitricity.carparkubi.service.impl.ChargingPointServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
 public class ChargingPointServiceTest {
 
-    @Mock
     private ChargingPointService service;
+
+    @Mock
+    private ChargingPointRepository repo;
 
     @Before
     public void init(){
-        service = Mockito.mock(ChargingPointService.class, Mockito.RETURNS_MOCKS);
-        Mockito.when(service.getChargingPoint())
-                .thenReturn(Stream.of(
-                        new ChargingPoint(ChargingPointId.CP1),
-                        new ChargingPoint(ChargingPointId.CP2),
-                        new ChargingPoint(ChargingPointId.CP3),
-                        new ChargingPoint(ChargingPointId.CP4),
-                        new ChargingPoint(ChargingPointId.CP5),
-                        new ChargingPoint(ChargingPointId.CP6),
-                        new ChargingPoint(ChargingPointId.CP7),
-                        new ChargingPoint(ChargingPointId.CP8),
-                        new ChargingPoint(ChargingPointId.CP9),
-                        new ChargingPoint(ChargingPointId.CP10)
+        repo = Mockito.mock(ChargingPointRepository.class, Mockito.RETURNS_MOCKS);
+
+        Mockito.when(repo.findAll())
+                .thenReturn(Arrays.asList(
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP1)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP2)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP3)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP4)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP5)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP6)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP7)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP8)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP9)),
+                        ChargingPointDTO.from(new ChargingPoint(ChargingPointId.CP10))
                 ));
+
+        service = new ChargingPointServiceImpl(repo);
     }
 
     @Test
-    public void getTotalUsedAmperes(){
+    public void getTotalUsedAmperesTest(){
         assertEquals(0, service.getTotalUsedAmperes().intValue());
     }
 }
